93dc0897f677fc434387805597f42e7c9be5e872
Jayasekara Kapukotuwa <ncbdrck@hotmail.com>
2024-12-20 08:54:00 +0000

    working now

---
+++ policy/adaptation_based.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+"""
+Adaptation-based policy
+- Each environment has its own adaption network
+- actor and critic networks are shared across environments
+- we need to multiply the max_action to the output of the actor network after calling the actor network
+- we need to divide the actions by the max_action in the critic network before calling the critic network
+- "act_dim" is the maximum action space of all environments. So we need to pad the action space of environments with smaller action space
+"""
+
+# Adaptation network for task-specific processing
+class AdaptationNetwork(nn.Module):
+    def __init__(self, env_params, output_size=128):
+        super(AdaptationNetwork, self).__init__()
+        self.fc1 = nn.Linear(env_params['obs'] + env_params['goal'], 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, output_size)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        return x
+
+# define the actor network
+class actor(nn.Module):
+    def __init__(self, input_size=128, act_dim=7):
+        super(actor, self).__init__()
+        self.fc1 = nn.Linear(input_size, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 256)
+        self.action_out = nn.Linear(256, act_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        actions = torch.tanh(self.action_out(x))
+
+        return actions
+
+class critic(nn.Module):
+    def __init__(self):
+        super(critic, self).__init__()
+        self.max_action = env_params['action_max']
+        self.fc1 = nn.Linear(env_params['obs'] + env_params['goal'] + env_params['action'], 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 256)
+        self.q_out = nn.Linear(256, 1)
+
+    def forward(self, x, actions):
+        x = torch.cat([x, actions / self.max_action], dim=1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        q_value = self.q_out(x)
+
+        return q_value
